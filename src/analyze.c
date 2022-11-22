17bf77737e8bdc73873b24a6fc1fd44f356b0067
ArturAssisComp <arturassiscomp@gmail.com>
2022-11-21 21:11:25 -0300

    Implement function to initiate symbol table, create key, extract name and partially implemented the function for building symbol table

--- src/analyze.c
+++ src/analyze.c
@@ -1,13 +1,25 @@
 #include <stdlib.h>
 #include <string.h>
-//#include "symtab.h"
+#include "symbol_table.h"
 #include "analyze.h"
 #include "aux.h"
 
 /* scope variable of studied line */
 static int   scope_id = 0;
 
-static char *append_to_scope(char *scope, char* name) {
+static int try_to_evaluate_const_expression(G_tree_node *t)
+{
+    //TODO: Implement
+    return -1;
+}
+
+static void semantic_error(int lineno, char * message)
+{ 
+    fprintf(G_listing,"Semantic error at line %d: %s\n",lineno,message);
+    G_error = true;
+}
+
+char *append_to_scope(char *scope, char* name) {
     char *result;
     size_t sz = strlen(scope) + strlen(name) + snprintf(NULL, 0, "%d", scope_id + 1) + 3;
     result = calloc(sz, sizeof *result);
@@ -19,6 +31,33 @@ static char *append_to_scope(char *scope, char* name) {
 
     result[0] = '\0';
     snprintf(result, sz, "%s$%s%%%d", scope, name, ++scope_id);
+    return result;
+}
+
+char *ANA_extract_name(char *id)
+{
+    char *result;
+    size_t i, id_len = strlen(id);
+    for(i = id_len - 1; i >= 0; i--)
+    {
+        if (id[i] == '@') break;
+    }
+    if(i < 0)
+    {
+        fprintf(G_listing,"Invalid id at line %d\n",G_lineno);
+        exit(EXIT_FAILURE);
+    }
+    size_t sz = id_len - i;
+    result = calloc(sz, sizeof *result);
+    if (!result)
+    {
+        fprintf(G_listing,"Out of memory error at line %d\n",G_lineno);
+        exit(EXIT_FAILURE);
+    }
+
+    result[0] = '\0';
+    strncpy(result, id + i + 1, sz - 1);
+    result[sz] = '\0';
     return result;
 }
 
@@ -43,10 +82,14 @@ static char *remove_scope(char* scope) {
     return result;
 }
 
+char *ANA_create_key(char *name, char *scope)
+{
+    return A_append(scope, "@", name);
+}
+
 
 static void traverse_to_define_scope(G_tree_node *t, char *scope)
 {
-    int i;
     char *aux_scope = NULL, *tmp = NULL;
     if (t)
     {
@@ -147,234 +190,209 @@ static void traverse_to_define_scope(G_tree_node *t, char *scope)
 }
 
 
-
-/* Procedure traverse is a generic recursive 
- * syntax tree traversal routine:
- * it applies preProc in preorder and postProc 
- * in postorder to tree pointed to by t
- */
-/*
-static void traverse( G_tree_node * t,
-               void (* pre_proc) (G_tree_node *),
-               void (* post_proc) (G_tree_node *) )
-{ if (t != NULL)
-  { pre_proc(t);
-    { int i;
-      for (i=0; i < MAXCHILDREN; i++)
-        traverse(t->child[i],pre_proc,post_proc);
+static SYM_row_entry *create_row_entry(G_tree_node *t)
+{
+    SYM_row_entry *result = NULL;
+    G_tree_node *tmp = NULL;
+    int i;
+    result = malloc(sizeof *result);
+    if (!result)
+    {
+        fprintf(G_listing,"(create_row_entry)Out of memory error at line %d\n",G_lineno);
+        exit(EXIT_FAILURE);
     }
-    post_proc(t);
-    traverse(t->sibling,pre_proc,post_proc);
-  }
-}
-*/
-
-/* nullProc is a do-nothing procedure to 
- * generate preorder-only or postorder-only
- * traversals from traverse
- */
-/*
-static void null_proc(G_tree_node * t)
-{ if (t==NULL) return;
-  else return;
-}
-*/
-
-/* Procedure insertNode inserts 
- * identifiers stored in t into 
- * the symbol table 
- */
-/*************************************************************
-static void insert_node( G_tree_node * t)
-{ 
-    switch (t->node_type)
+    result->id = ANA_create_key(t->attr.name, t->scope);
+    result->name = A_copy_string(t->attr.name);
+    result->scope = A_copy_string(t->scope);
+    result->is_parameter = false;
+    result->lineno = t->lineno;
+    result->semantic_type = t->semantic_type;
+    if (t->node_type == G_STMT)
     { 
-        case G_STMT:
-            switch (t->node_subtype)
-            { 
-                case G_ASSIGNMENT:
-                    if !(SYM_var_isin_within(t->child[0], scope)){
-                      type_error(t, t->child[0]->attr.name);
-                  }
-                  break;
-              case G_VAR_DCL:
-                  if (SYM_var_isin_exact(t, scope)){
-                      type_error(t, t->attr.name);
-                  } else if (SYM_fun_isin(t, scope)) {
-                      type_error(t, t->attr.name);
-                  } else {
-                      SYM_row_entry e = SYM_create_entry(t, scope);
-                      if( !SYM_add_definition_to_symbol_table(e) )
-                          // Nothing for instance
-                  }
-                  break;
-              case G_FUNC_DCL:
-                  if (strcmp(scope, '') || SYM_var_isin_exact(t, scope))
-                      type_error(t, t->attr.name);
-                  SYM_row_entry e = SYM_create_entry(t, scope);
-                  SYM_add_definition_to_symbol_table(e);
-                  append_to_scope(t->attr.name);
-                  break;
-              case G_IF:
-                  // Em teoria não pode porque tem a expressão que não está no scope do if
-                  append_to_scope(t->attr.name);
-                  break;
-              case G_WHILE:
-                  append_to_scope(t->attr.name);
-                  break;
-              case G_RETURN:
-                  break;
-              case G_PARAM:
-                  if (SYM_var_isin_exact(t, scope)){
-                      type_error(t, t->attr.name);
-                  } else if (SYM_fun_isin(t, scope)) {
-                      type_error(t, t->attr.name);
-                  } else {
-                      SYM_row_entry e = SYM_create_entry(t, scope);
-                      SYM_add_definition_to_symbol_table(e);
-                  }
-                  break;
-              default:
+        switch (t->node_subtype.stmt) 
+        {
+            case G_VAR_DCL:
+                if (t->array_sz > 0)
+                {
+                    result->specific_attr.arr.size = t->array_sz;
+                    result->specific_attr.arr.offset = -1;
+                    result->id_type = SYM_ARRAY_VAR;
+                }
+                else result->id_type = SYM_VAR;
                 break;
-            }
-      break;
-    case G_EXP:
-      switch (t->node_subtype)
-      { //case IdK:
-    //       if (st_lookup(t->attr.name) == -1)
-    //         st_insert(t->attr.name,t->lineno,location++);
-    //       else
-    //         st_insert(t->attr.name,t->lineno,0);
-    //       break;
-        case G_FUNC_ACTV:
-            if !(SYM_fun_isin(t, scope)) {
-                type_error(t, t->attr.name);
-            else {
-                SYM_insert_line_occurrence_into_symbol_table(t->attr.name, scope, )
-            }
-            break;
-        case G_ID:
-        case G_ARRAY_ID:
-            if (SYM_fun_isin(t, scope)) {
-                type_error(t, t->attr.name);
-            else if !(SYM_var_isin_within(t, scope)) {
-                type_error(t, t->attr.name);
-            } else {
-                SYM_insert_line_occurrence_into_symbol_table(t->attr.name, scope, )
-            }
-            break;
-        case G_COMP: // Ok
-        case G_OP: // Ok
-        case G_CONST: // OK
-        default:
-            break;
+            case G_PARAM:
+                if (t->array_sz == 0)
+                {
+                    result->specific_attr.arr.size = t->array_sz;
+                    result->specific_attr.arr.offset = -1;
+                    result->id_type = SYM_ARRAY_VAR;
+                }
+                else result->id_type = SYM_VAR;
+
+                result->is_parameter = true;
+                break;
+            case G_FUNC_DCL:
+                result->id_type = SYM_FUNC;
+                i = 0;
+                tmp = t->child[0];
+                while (tmp)
+                {
+                    result->specific_attr.func.args_keys[i++] = ANA_create_key(tmp->attr.name, tmp->scope); 
+                    tmp = tmp->sibling;
+                }
+                result->specific_attr.func.num_of_args = i;
+                break;
+            default:
+                fprintf(G_listing,"Invalid ExpNode kind\n");
+                exit(EXIT_FAILURE);
+                break;
+        }
+    }
+    else if (t->node_type == G_EXP)
+    { 
+        switch (t->node_subtype.exp) {
+            case G_COMP:
+            case G_OP:
+            case G_CONST:
+            case G_FUNC_ACTV:
+                result->id_type = SYM_FUNC;
+                break;
+            case G_ID:
+                result->id_type = SYM_VAR;
+                break;
+            case G_ARRAY_ID:
+                result->specific_attr.arr.offset = try_to_evaluate_const_expression(t->child[0]);
+                result->id_type = SYM_ARRAY_VAR;
+                break;
+            default:
+                fprintf(G_listing,"Unknown ExpNode kind\n");
+                exit(EXIT_FAILURE);
+                break;
       }
-        break;
-    default:
-        break;
-  }
+    }
+    else 
+    {
+        fprintf(G_listing,"INVALID NODE\n");
+        exit(EXIT_FAILURE);
+    }
 }
-*************************************************************/
+
+static void free_row_entry(SYM_row_entry **ptr_r)
+{
+    int i;
+    SYM_row_entry *t = *ptr_r;
+    *ptr_r = NULL;
+    free(t->id);
+    free(t->name);
+    free(t->scope);
+    if (t->id_type == SYM_FUNC)
+    {
+        for (i = 0; i < t->specific_attr.func.num_of_args; i++) free(t->specific_attr.func.args_keys[i]); 
+    }
+}
+
+
+static void traverse_to_init_symbol_table(G_tree_node *t)
+{
+    SYM_row_entry *aux_entry;
+    if (t)
+    {
+        if (t->node_type == G_STMT)
+        { 
+            switch (t->node_subtype.stmt) 
+            {
+                case G_VAR_DCL:
+                case G_PARAM:
+                    aux_entry = create_row_entry(t);
+                    if(SYM_there_is_function_name_conflict(aux_entry->name))
+                    {
+                        //semantic error - there is already a function with the same name
+                        semantic_error(t->lineno, A_append("There is already a declaration of a function with name \"", t->attr.name, "\"."));
+                        exit(EXIT_FAILURE);
+                    }
+                    if(!SYM_add_definition_to_symbol_table(aux_entry))
+                    {
+                        //semantic error - multiple declarations in the same scope
+                        semantic_error(t->lineno, A_append("Multiple declarations of \"", t->attr.name, "\" in the same scope."));
+                        exit(EXIT_FAILURE);
+                    }
+                    free_row_entry(&aux_entry);
+                    break;
+                case G_FUNC_DCL:
+                    aux_entry = create_row_entry(t);
+                    if(SYM_there_is_name_conflict(aux_entry->name))
+                    {
+                        //semantic error - there is already a function/variable with the same name
+                        semantic_error(t->lineno, A_append("There is already a declaration with name \"", t->attr.name, "\"."));
+                        exit(EXIT_FAILURE);
+                    }
+                    if(!SYM_add_definition_to_symbol_table(aux_entry))
+                    {
+                        //semantic error - multiple declarations in the same scope
+                        semantic_error(t->lineno, A_append("Multiple declarations of \"", t->attr.name, "\" in the same scope."));
+                        exit(EXIT_FAILURE);
+                    }
+                    free_row_entry(&aux_entry);
+                    traverse_to_init_symbol_table(t->child[0]);
+                    traverse_to_init_symbol_table(t->child[1]);
+                    break;
+                case G_BLOCK:
+                    traverse_to_init_symbol_table(t->child[0]);
+                    break;
+                case G_IF:
+                    traverse_to_init_symbol_table(t->child[1]);
+                    traverse_to_init_symbol_table(t->child[2]);
+                    break;
+                case G_WHILE:
+                    traverse_to_init_symbol_table(t->child[1]);
+                    break;
+                case G_RETURN:
+                case G_ASSIGNMENT:
+                    break;
+                default:
+                    fprintf(G_listing,"Unknown ExpNode kind\n");
+                    break;
+            }
+        }
+        else if (t->node_type == G_EXP)
+        { 
+            switch (t->node_subtype.exp) {
+                case G_COMP:
+                case G_OP:
+                case G_FUNC_ACTV:
+                case G_CONST:
+                case G_ID:
+                case G_ARRAY_ID:
+                    break;
+                default:
+                    fprintf(G_listing,"Unknown ExpNode kind\n");
+                    break;
+          }
+        }
+        else fprintf(G_listing,"INVALID NODE\n");
+
+        traverse_to_init_symbol_table(t->sibling);
+    }
+}
+
 
 void ANA_set_syntax_tree_scope(G_tree_node *root)
 {
-    char *t = A_copy_string("GLOBAL%0");
+    char *t = A_copy_string(GLOBAL_PREFIX);
     scope_id = 0;
     traverse_to_define_scope(root, t);
 }
 
-/* Function buildSymtab constructs the symbol 
- * table by preorder traversal of the syntax tree
- */
-/*
-void build_symtab(G_tree_node * syntaxTree)
-{ traverse(syntaxTree,insert_node,null_proc);
-  if (TraceAnalyze)
-  { fprintf(listing,"\nSymbol table:\n\n");
-    printSymTab(listing);
-  }
+void ANA_init_symbol_table(G_tree_node *root)
+{ 
+    SYM_init_symbol_table();
+    traverse_to_init_symbol_table(root);
+    //traverse_to_check_for_scope_errors(root, t);
+    //traverse_to_check_for_type_errors(root, t);
+    if (G_trace_analyze)
+    { 
+        fprintf(G_listing,"\nSymbol table:\n\n");
+        SYM_print_symbol_table(G_listing);
+    }
 }
-*/
 
-/*
-static void type_error(G_tree_node * t, char * message)
-{ fprintf(G_listing,"Type error at line %d: %s\n",t->lineno,message);
-  G_error = true;
-}
-*/
-
-/* Procedure checkNode performs
- * type checking at a single tree node
- */
-/*
-static void check_node(G_tree_node * t)
-{ switch (t->node_type)
-  { case G_EXP:
-      switch (t->node_subtype)
-      { // case OpK:
-        //   if ((t->child[0]->type != Integer) ||
-        //       (t->child[1]->type != Integer))
-        //     typeError(t,"Op applied to non-integer");
-        //   if ((t->attr.op == EQ) || (t->attr.op == LT))
-        //     t->type = Boolean;
-        //   else
-        //     t->type = Integer;
-        //   break;
-        // case ConstK:
-        // case IdK:
-        //   t->type = Integer;
-        //   break;
-        case G_COMP:
-        case G_OP:
-        case G_FUNC_ACTV:
-        case G_CONST:
-        case G_ID:
-        case G_ARRAY_ID:
-        default:
-          break;
-      }
-      break;
-    case G_STMT:
-      switch (t->node_subtype)
-      { // case IfK:
-        //   if (t->child[0]->type == Integer)
-        //     typeError(t->child[0],"if test is not Boolean");
-        //   break;
-        // case AssignK:
-        //   if (t->child[0]->type != Integer)
-        //     typeError(t->child[0],"assignment of non-integer value");
-        //   break;
-        // case WriteK:
-        //   if (t->child[0]->type != Integer)
-        //     typeError(t->child[0],"write of non-integer value");
-        //   break;
-        // case RepeatK:
-        //   if (t->child[1]->type == Integer)
-        //     typeError(t->child[1],"repeat test is not Boolean");
-        //   break;
-        case G_ASSIGNMENT:
-        case G_VAR_DCL: 
-        case G_FUNC_DCL:
-        case G_IF:
-        case G_WHILE:
-        case G_RETURN: 
-        case G_PARAM:
-        default:
-          break;
-      }
-      break;
-    default:
-      break;
-
-  }
-}
-*/
-
-/* Procedure typeCheck performs type checking 
- * by a postorder syntax tree traversal
- */
-/*
-void type_check(G_tree_node * syntax_tree)
-{ traverse(syntax_tree,null_proc,check_node);
-}
-*/
--- src/analyze.h
+++ src/analyze.h
@@ -3,16 +3,19 @@
 
 #include "globals.h"
 
-/* Function buildSymtab constructs the symbol 
- * table by preorder traversal of the syntax tree
+/*It builds the symbol table, checking for scope errors, type
+ * errors and semantic errors in general.
  */
-void ANA_build_symtab(G_tree_node *);
+void ANA_init_symbol_table(G_tree_node *);
 
-/* Procedure typeCheck performs type checking 
- * by a postorder syntax tree traversal
- */
-void ANA_type_check(G_tree_node *);
 
+//Returns the key created from the combination of the name 'name' and the 
+//scope 'scope'.
+char *ANA_create_key(char *name, char *scope);
+
+//Returns an allocated string with the name
+//extracted from id.
+char *ANA_extract_name(char *id);
 
 void ANA_set_syntax_tree_scope(G_tree_node *root);
 #endif
