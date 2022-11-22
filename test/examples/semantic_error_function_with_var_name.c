9460a878a6b25ddeece8540a32f1502cb68d39bc
ArturAssisComp <arturassiscomp@gmail.com>
2022-11-21 21:13:16 -0300

    Add test files

---
+++ test/examples/semantic_error_function_with_var_name.c
@@ -0,0 +1,16 @@
+
+int foo(void)
+{
+    {int i;}
+    return 0;
+}
+
+int i(int j)
+{
+    return 1;
+}
+
+int main(void)
+{
+    return 0;
+}
---
+++ test/examples/semantic_error_var_with_function_name.c
@@ -0,0 +1,13 @@
+
+void foo(int i)
+{
+    return ;
+}
+int main(void)
+{
+    int foo;
+    int i;
+    i = 12;
+    foo = 2;
+    return i + foo;
+}
